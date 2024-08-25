d58310dd5239245ef0db40b3743d5a72fa927eda
nikita nimore <992nikitanimore@gmail.com>
2024-08-25 23:08:20 +0530

    first commit and first trayal program

---
+++ helllo.java
@@ -0,0 +1,8 @@
+// import java.util.Scanner;
+
+class student {
+    public static void main(String[] args) {
+        // Scanner sk = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
+}
