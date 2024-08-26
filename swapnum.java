9dd88699f245e20a18bc9f6637f28e25b9ece9b7
nikita nimore <992nikitanimore@gmail.com>
2024-08-26 23:30:54 +0530

    complete my program swap two num

--- swapnum.java
+++ swapnum.java
@@ -1,2 +1,16 @@
 
 
+public class swapnum{
+    public static void main(String[] args){
+        int a , b , c;
+         a = 10;
+         b = 20;
+         c = a;
+         a = b;
+         b = c;
+         System.out.println("a = "+a+" b = "+b);
+
+
+
+    }
+}
