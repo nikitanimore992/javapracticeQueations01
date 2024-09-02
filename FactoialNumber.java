b856539e669e96c3167938390238ee782a7444c7
nikita nimore <992nikitanimore@gmail.com>
2024-09-02 23:53:43 +0530

    generate factorial is successfully

--- FactoialNumber.java
+++ FactoialNumber.java
@@ -3,7 +3,10 @@
 
 public class FactoialNumber{
     public static void main(String[] args){
-       
-        
+        int num = 5 , fact = 1;
+        for (int i = 1; i<= num; i++){
+            fact = fact*i;
+        }
+        System.out.print(fact);
     }
 }
