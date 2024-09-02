84b89e91799c3d139f730216544da618b76eb063
nikita nimore <992nikitanimore@gmail.com>
2024-09-02 23:38:56 +0530

    create plaindrome String

---
+++ PalindromString.java
@@ -0,0 +1,11 @@
+
+
+
+public class PalindromString{
+    public static void main(String[] args){
+        String str = "nayan";
+        String palindrome = new StringBuilder(str).reverse().toString();
+        System.out.println(palindrome);
+       
+    }
+}
