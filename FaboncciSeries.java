abb1b6c1d62aaf8f9c1eec242cb2cc1b26d249c5
nikita nimore <992nikitanimore@gmail.com>
2024-09-02 23:47:26 +0530

    create a fabonacci serice

--- FaboncciSeries.java
+++ FaboncciSeries.java
@@ -4,7 +4,14 @@
 public class FaboncciSeries{
     public static void main(String[] args){
 
-
-        
+        int n = 10, num1 = 0, num2 = 2;
+        System.out.print("Fabonacci serice : "+num1 + ","+num2);{
+            for(int i=2; i<n; i++){
+                int num3 = num1+num2;
+                System.out.print(","+num3);
+                num1=num2;
+                num2=num3;
+                }
+        }
     }
 }
