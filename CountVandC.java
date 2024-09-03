a6e4d5803e0c83071648da8c4decd8282243b5b3
nikita nimore <992nikitanimore@gmail.com>
2024-09-04 00:14:51 +0530

    complete my code

--- CountVandC.java
+++ CountVandC.java
@@ -2,8 +2,19 @@
 
 public class CountVandC{
     public static void main(String[] args){
-            
 
+            String str = "nikita";
+            int v = 0, con = 0;
 
-    }
+            for(char c:str.toCharArray()){
+                if("aeiouAEIOU".indexOf(c)!=-1){
+                    v++;
+
+                }else if(Character.isLetter(c)){
+                    con++;
+                }
+            }
+                System.out.println("vowel : "+v+" , consonats : "+con);
+        }
+
 }
