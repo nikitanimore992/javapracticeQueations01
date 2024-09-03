74942f49f05fdde7124636cd761b59c8dbf6f028
nikita nimore <992nikitanimore@gmail.com>
2024-09-04 00:20:29 +0530

    complete may ShortArray program

--- ShortArray.java
+++ ShortArray.java
@@ -1,7 +1,9 @@
-
+import java.util.Arrays;
 
 public class ShortArray{
     public static void main(String[] args){
-        
+        int arr[] = {2 , 3 , 8 , 7};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
