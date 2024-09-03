69a5e9b76988f6192a7e7715753697794064e282
nikita nimore <992nikitanimore@gmail.com>
2024-09-04 00:25:05 +0530

    remark

--- ShortArray.java
+++ ShortArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class ShortArray{
     public static void main(String[] args){
-        int arr[] = {2 , 3 , 8 , 7};
+        int arr[] = {2 , 3 , 8 , 7 , 50 , 25 , 10 , 01 , 5};
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
