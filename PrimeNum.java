a9fbe808593fd27243972ab384a85bb13d2c22e4
nikita nimore <992nikitanimore@gmail.com>
2024-09-03 23:46:00 +0530

    cmplete prime num page

--- PrimeNum.java
+++ PrimeNum.java
@@ -3,6 +3,15 @@
 
 public class PrimeNum{
     public static void main(String[] args){
-        
+
+            int num = 11;
+            boolean isPrime = true;
+             for (int i =2; i<=Math.sqr(num); i++){
+                if(num%i==0){
+                    isPrime = false;
+                    break;
+                }
+             }
+             System.out.println(isPrime);
     }
 }
