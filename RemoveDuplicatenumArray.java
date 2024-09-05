88b03f1958a901f154e69c89fbe98a5b41205816
nikita nimore <992nikitanimore@gmail.com>
2024-09-05 15:46:19 +0530

    RemoveDuplicatenumArray complete program

--- RemoveDuplicatenumArray.java
+++ RemoveDuplicatenumArray.java
@@ -1,7 +1,14 @@
 
-
+import java.util.HashSet;
 class RemoveDuplicatenumArray{
     public static void main(String[] args){
-        
+
+
+            int arr[] = {11,1,15,2,68,5,4,6,9,1,83,45};
+            HashSet<Integer>set= new HashSet<>();
+            for(int num : arr){
+                set.add(num);
+            }
+            System.out.print(set);
     }
 }
