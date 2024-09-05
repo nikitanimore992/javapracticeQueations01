5580738688561762a0713837637bfc8d73d93ee0
nikita nimore <992nikitanimore@gmail.com>
2024-09-05 15:38:35 +0530

    FindlargestNumArray program complete

--- FindlargestNumArray.java
+++ FindlargestNumArray.java
@@ -3,6 +3,15 @@
 
 class FindlargestNumArray{
     public static void main(String[] args){
-        
+      
+       int arr[]={1,2,3,4,5,6,12,18,20};
+       int larg = arr[0];
+       for(int num:arr){
+        if(num>larg){
+            larg= num;
+        }
+       } 
+
+        System.out.println(larg);
     }
 }
