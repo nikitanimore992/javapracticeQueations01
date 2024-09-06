8b363f3d14e7762c041b6e798673b20e99497b51
nikita nimore <992nikitanimore@gmail.com>
2024-09-06 23:38:24 +0530

    complete program calculategcd

--- calculateGCD.java
+++ calculateGCD.java
@@ -3,7 +3,14 @@
 
 class calculateGCD{
     public static void main(String[] args){
+int a = 60; 
+int b = 48;
+while(b!=0){
+    int temp = b;
+    b = a%b;
+    a = temp;
 
-        
+}
+    System.out.print(a);
     }
 }
