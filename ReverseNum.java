0e8ad802664cf1fbfa158d9a5f57b8f7b4f85cdb
nikita nimore <992nikitanimore@gmail.com>
2024-09-06 23:32:52 +0530

    program complete

--- ReverseNum.java
+++ ReverseNum.java
@@ -4,6 +4,12 @@
 public class ReverseNum{
     public static void main(String[] args){
         int num = 123;
-        
+        int res = 0;
+        while(num!=0){
+           
+            res = res * 10 + num%10;
+            num /=10;
+        }
+        System.out.println(res);
     }
 }
