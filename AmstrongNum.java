f96d7b607b4bc6bcd656ae2be62ffa1aa3b30844
nikita nimore <992nikitanimore@gmail.com>
2024-09-06 23:21:07 +0530

    complete program

--- AmstrongNum.java
+++ AmstrongNum.java
@@ -1,10 +1,14 @@
 
-
-
 class AmstrongNum{
     public static void main(String args[]){
+        int num = 153 , sum = 0;
+        int temp = num;
+        while(temp!=0){
+            int digit = temp  %10;
+            sum += Math.pow(digit,3);
+            temp = temp/10;
+        }
 
-
-        
+            System.out.println(num==sum);
     }
 }
