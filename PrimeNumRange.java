6a6bbea6c6cc60bfe0ca910f9461e519e73c732b
nikita nimore <992nikitanimore@gmail.com>
2024-09-07 23:11:06 +0530

    program complete

--- PrimeNumRange.java
+++ PrimeNumRange.java
@@ -2,6 +2,21 @@
 
 class PrimeNumRange{
     public static void main(String[] args){
+       
         
+        int strt = 10;
+        int end = 50;
+        for (int num = strt; num<=end; num++){
+            boolean isPrime = true;
+            for (int i =2; i<=Math.sqrt(num); i++){
+                if(num%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime&&num>1){
+                System.out.println(num+" ");
+            }
+        }
     }
 }
