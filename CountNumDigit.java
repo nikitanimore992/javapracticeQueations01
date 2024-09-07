eb12e6dbfdf53afd540a66e0b5067e267e694e7d
nikita nimore <992nikitanimore@gmail.com>
2024-09-07 23:00:36 +0530

    complete count num digit program

--- CountNumDigit.java
+++ CountNumDigit.java
@@ -1,9 +1,11 @@
 
 
 
-public CountNumDigit{
+public class CountNumDigit{
     public static void main(String[] args){
 
-        
+        int num = 1234561805;
+        int count = String.valueOf(num).length();
+        System.out.println(count);
     }
 }
