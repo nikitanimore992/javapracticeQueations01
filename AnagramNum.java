ad092da1d9f739dbcc458ea759d3976d9dd1469a
nikita nimore <992nikitanimore@gmail.com>
2024-09-07 22:47:51 +0530

    program complete

--- AnagramNum.java
+++ AnagramNum.java
@@ -1,8 +1,18 @@
 
-
+import java.util.Arrays;
 public class AnagramNum{
     public static void main(String[] args){
 
-        
+            String str1 = "nikita";
+            String str2 = "Anjali";
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            System.out.println(Arrays.equals(arr1 , arr2));
+
+
     }
 }
