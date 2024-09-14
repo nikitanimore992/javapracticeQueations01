ed57cdc6cee6a6a69cce4721ee6ee6c29de3dc9d
nikita nimore <992nikitanimore@gmail.com>
2024-09-14 14:55:43 +0530

    proram complete

--- StringInout.java
+++ StringInout.java
@@ -1,7 +1,11 @@
 
-
-public class {
+import java.util.Scanner;
+class StringInout  {
     public static void main(String[] args){
-        
+        String str;
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        str = sk.nextLine();
+        System.out.println("Your name is : "+str);
     }
 }
