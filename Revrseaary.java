3302e29986552beb15ce36331a61fd87b7268dd5
nikita nimore <992nikitanimore@gmail.com>
2024-11-04 23:33:56 +0530

    complete my reverse array program

--- Revrseaary.java
+++ Revrseaary.java
@@ -1,1 +1,22 @@
-Revrseaary.java
+
+
+public class Revrseaary {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int start = 0;
+        int end = numbers.length - 1;
+while (start < end) {
+            // Swap elements at start and end indices
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            // Move indices towards the center
+            start++;
+            end--;
+        }
+        // Print reversed array
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
