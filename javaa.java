280a597069d0b1181253fe2ca668fb61b90f6f67
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-01-01 23:06:02 +0530

    Update
    create a new java file

---
+++ javaa.java
@@ -0,0 +1,18 @@
+public class GFG {
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Declaring an integer variable
+        int x = 2;
+        // For loop to iterate
+        for (long y = 0, z = 4; x < 10 && y < 10;
+             x++, y++) {
+            // Printing value/s stored in variable named y
+            // defined inside body of for loop
+            System.out.println(y + " ");
+        }
+        // Printing value/s stored in variable named x
+        // defined outside body of for loop
+        System.out.println(x);
+    }
+}
