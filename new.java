ad5bb7234ea815148f9dde42f6256acc6e2645f9
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-02-25 23:32:57 +0530

    Update new.java

--- new.java
+++ new.java
@@ -1,1 +1,16 @@
-create a java file
+public class Main {
+    public static void main(String[] args) {
+        int[] my_array = {7, 12, 9, 4, 11};
+        int minVal = my_array[0];
+
+        for (int i : my_array) {
+            if (i < minVal) {
+                minVal = i;
+            }
+        }
+
+        System.out.println("Lowest value: " + minVal);
+    }
+}
+
+//Java
