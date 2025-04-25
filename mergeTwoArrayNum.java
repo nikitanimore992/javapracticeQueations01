dc99549dcde94a24c4fe2921cd5a654ae1d3376f
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-04-25 21:14:20 +0530

    Update mergeTwoArrayNum.java

--- mergeTwoArrayNum.java
+++ mergeTwoArrayNum.java
@@ -3,7 +3,6 @@
 import java.util.Arrays;
 public class mergeTwoArrayNum{
     public static void main (String[] args){
-
         int arr1[]= {1,2,3};
         int arr2[]={4,5,6};
         int fal = arr1.length;
