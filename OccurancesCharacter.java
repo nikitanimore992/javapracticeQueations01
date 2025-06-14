50449707d8e875597c2aa0f8a6de227ad4840f24
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-06-14 23:09:27 +0530

    Update OccurancesCharacter.java

--- OccurancesCharacter.java
+++ OccurancesCharacter.java
@@ -1,6 +1,10 @@
 public class OccurancesCharacter{
     public static void main(String args[]){
         String str = "Automationaa";
+
+
+
+
         char ch = 'a';
         int count = 0;
         for(char c : str.toCharArray()){
