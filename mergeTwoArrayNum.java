46cc6b5725ae6c12531c6223ec65c24a19c8c96d
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-09-05 23:39:08 +0530

    Update mergeTwoArrayNum.java

--- mergeTwoArrayNum.java
+++ mergeTwoArrayNum.java
@@ -7,7 +7,6 @@ public class mergeTwoArrayNum{
         int arr2[]={4,5,6};
         int fal = arr1.length;
         int sal = arr2.length;
-
         int[] merged = new int[fal+sal];
         System.arraycopy(arr1,0,merged,0,arr1.length);
         System.arraycopy(arr2,0,merged,arr1.length,arr2.length);
