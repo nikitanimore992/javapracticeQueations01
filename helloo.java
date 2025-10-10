583f31ce0bab955b2baa09df78df9b5bbaeb7983
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-10 14:30:04 +0530

    add something

--- helloo.java
+++ helloo.java
@@ -1,3 +1,22 @@
-class Helloo{
-
+class helloo {
+    public static void main(String [ ]args){
+     int marks = 95;
+     if (marks >=90){
+        System.out.println("A+");
+     
+         else if (marks>= 80){
+            System.out.println("A");
+        }
+            else if (marks >=75)
+            {
+                System.out.println("B+");
+            }
+               
+                } else {
+                    System.out.println("fail");
+                }
+          
+     
+    }
 }
+
