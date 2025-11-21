20c6f97ec17c3e6da953c45b778a2adf79fd99fd
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-11-21 23:00:51 +0530

    Update FaboncciSeries.java

--- FaboncciSeries.java
+++ FaboncciSeries.java
@@ -7,7 +7,7 @@ public class FaboncciSeries{
                 int num3 = num1+num2;
                 System.out.print(","+num3);
                 num1=num2;
-               num2=num3;
+              num2=num3;
            }
        }
     }
