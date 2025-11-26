028f99f1758e5840d4386a6b4536fb7c89bc9e1b
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-11-26 23:43:27 +0530

    Update FaboncciSeries.java

--- FaboncciSeries.java
+++ FaboncciSeries.java
@@ -1,7 +1,8 @@
 public class FaboncciSeries{
     public static void main(String[] args){
        int n = 10, num1 = 0, num2 = 2;
-        System.out.print("Fabonacci serice : "+num1 + ","+num2);{
+        System.out.print("Fabonacci serice : "+num1 + ","+num2);
+{
             for(int i=2; i<n; i++){
                 int num3 = num1+num2;
                 System.out.print(","+num3);
