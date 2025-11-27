559089b47ffb4b2d029691a5074e602ecfbf374f
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-11-27 23:43:32 +0530

    Update PrimeNum.java

--- PrimeNum.java
+++ PrimeNum.java
@@ -1,6 +1,5 @@
 public class PrimeNum{
     public static void main(String[] args){
-
             int num = 11;
             boolean isPrime = true;
              for (int i =2; i<=Math.sqr(num); i++){
