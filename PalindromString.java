ad7211edb08be41bf649d7e0517fe06c8893b698
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-11-28 23:10:00 +0530

    Update PalindromString.java

--- PalindromString.java
+++ PalindromString.java
@@ -1,6 +1,5 @@
 public class PalindromString{
     public static void main(String[] args){
-
         String str = "nayan";
         String palindrome = new StringBuilder(str).reverse().toString();
        System.out.println(palindrome);
