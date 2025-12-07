57d229fb8fcf771600d02f660656f9836cf43a99
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2025-12-07 23:28:39 +0530

    Update CountVandC.java

--- CountVandC.java
+++ CountVandC.java
@@ -2,7 +2,6 @@ public class CountVandC{
     public static void main(String[] args){
            String str = "nikita";
             int v = 0, con = 0;
-
             for(char c:str.toCharArray()){
                 if("aeiouAEIOU".indexOf(c)!=-1){
                     v++;
