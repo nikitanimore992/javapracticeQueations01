0ab82841adaedd8203a03404bbeb38ffd18205a3
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2026-01-14 23:57:51 +0530

    Update MinMaxFinder.java

--- MinMaxFinder.java
+++ MinMaxFinder.java
@@ -7,7 +7,7 @@ public class MinMaxFinder {
         int mini = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             if (A[i] < mini) {
-                mini = A[i];
+               mini = A[i];
           }
         }
       return mini;
