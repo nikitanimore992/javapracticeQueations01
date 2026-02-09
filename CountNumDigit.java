c68c26da4368d44549fc5cca922f3e34d25e2d7d
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2026-02-09 23:59:35 +0530

    Update CountNumDigit.java

--- CountNumDigit.java
+++ CountNumDigit.java
@@ -1,7 +1,7 @@
 public class CountNumDigit{
    public static void main(String[] args){
        int num = 1234561805;
-       int count = String.valueOf(num).length();
+      int count = String.valueOf(num).length();
        System.out.println(count);
    }
 }
