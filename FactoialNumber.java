6fb198be10c025ffbb69e38c51c5cebf6a91d849
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2026-02-10 23:10:51 +0530

    Update FactoialNumber.java

--- FactoialNumber.java
+++ FactoialNumber.java
@@ -1,7 +1,7 @@
 
 public class FactoialNumber{
     public static void main(String[] args){
-       int num = 5 , fact = 1;
+      int num = 5 , fact = 1;
         for (int i = 1; i<= num; i++){
         fact = fact*i;
       } 
