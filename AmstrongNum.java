20cc7ba602fd46ca06c0a40b54ffa3cdc887e40d
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2026-02-11 23:33:20 +0530

    Update AmstrongNum.java

--- AmstrongNum.java
+++ AmstrongNum.java
@@ -2,7 +2,7 @@ class AmstrongNum{
  public static void main(String args[]){
        int num = 153 ;
        int sum = 0;
-       int temp = num;
+      int temp = num;
       while(temp!=0){
         int digit = temp  %10;
        sum += Math.pow(digit,3);
