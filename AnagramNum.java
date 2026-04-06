cfe53d084fe369f4ebb1db41ac23ddc4e8ee8b34
Nikita Nimore <154889425+nikitanimore992@users.noreply.github.com>
2026-04-06 23:54:17 +0530

    Update AnagramNum.java

--- AnagramNum.java
+++ AnagramNum.java
@@ -6,6 +6,7 @@ public class AnagramNum{
             char[] arr1 = str1.toCharArray();
             char[] arr2 = str2.toCharArray();
             Arrays.sort(arr1);
+
             Arrays.sort(arr2);
         System.out.println(Arrays.equals(arr1 ,arr2));
     }
